006f8660197bf0f2f45cfa5e7aa2444453dafb4c
maheshbasnet089 <basnetmanish089@gmail.com>
2024-09-19 21:04:09 +0545

    get api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,35 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import Card from "../components/Card"
 import Navbar from "../components/Navbar"
 import axios from "axios"
 
 
 function Home(){
+    const [blogs,setBlogs] = useState([])
     const fetchBlogs = async ()=>{
       const response =  await axios.get("http://localhost:3000/blog")
+      setBlogs(response.data.data)
     }
 
     useEffect(()=>{
         fetchBlogs()
     },[])
+    console.log(blogs)
+
+   
     return(    
         <>
             <Navbar/>
             <div className="flex flex-wrap">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {
+                blogs.map(function(blog){
+                    return(
+                        <Card blog={blog} />
+                    )
+                })
+            }
+     
+
             </div>
 
         </>
@@ -32,3 +39,5 @@ function Home(){
 }
 
 export default Home
+
+
