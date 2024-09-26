13f4a41793e5f2cdb7174d75293f623144d9ba90
maheshbasnet089 <basnetmanish089@gmail.com>
2024-09-26 20:48:34 +0545

    complete project

--- src/pages/EditBlog.jsx
+++ src/pages/EditBlog.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
 import axios from "axios"
 import { useNavigate, useParams } from "react-router-dom"
@@ -34,6 +34,25 @@ function EditBlog(){
         }
     }
 
+
+    const fetchSingleBlog = async ()=>{
+        const response  =   await axios.get("http://localhost:3000/blog/" + id)
+        if(response.status === 200){
+            setData({
+                title : response.data.data.title, 
+                description : response.data.data.description, 
+                subtitle : response.data.data.subtitle, 
+            })
+        }else{  
+            alert("something went wrong")
+        }
+       }
+   
+       useEffect(()=>{
+           fetchSingleBlog()
+       },[])
+
+       console.log(data)
     return (
       <>
        <Navbar />
@@ -44,15 +63,15 @@ function EditBlog(){
 
 <div class="p-8">
                 <div class="flex gap-4">
-                <input type="text" name="title" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Title *" onChange={handleChange}  />
+                <input type="text" name="title" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Title *" onChange={handleChange} value={data.title}  />
 
-                <input type="text" name="subtitle" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Subtitle *" onChange={handleChange}   />
+                <input type="text" name="subtitle" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Subtitle *" onChange={handleChange} value={data.subtitle}   />
                 </div>
                 <div class="my-6 flex gap-4">
                 <input type="file" name="image" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"  onChange={handleChange} />
                 </div>
                 <div class="">
-                <textarea name="description" id="text" cols="30" rows="10" class="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-gray-300" onChange={handleChange} >Description</textarea>
+                <textarea name="description" id="text" cols="30" rows="10" class="mb-10 h-40 w-full resize-none rounded-md border border-slate-300 p-5 font-semibold text-black" onChange={handleChange} value={data.description} >Description</textarea>
                 </div>
                 <div class="text-center">
                 <button class="cursor-pointer rounded-lg bg-blue-700 px-8 py-5 text-sm font-semibold text-white">Edit Blog</button>
